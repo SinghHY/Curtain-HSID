e57e1a7a468876c41a698c3d3d94315c94e23756
Harpreet Singh <Harpreet.Singh@perkinelmer.com>
2018-08-20 09:10:22 -0400

    XC16 Project created

---
+++ HSID_XC16.X/Main.c
@@ -0,0 +1,18 @@
+/* 
+ * File:   Main.c
+ * Author: SinghHy
+ *
+ * Created on August 20, 2018, 9:09 AM
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+
+/*
+ * 
+ */
+int main(int argc, char** argv) {
+
+    return (EXIT_SUCCESS);
+}
+
